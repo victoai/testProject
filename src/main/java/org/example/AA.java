910cec23ce7f6b83e51fc61db33b8fec26b68f6f
victoai <victoai@naver.com>
2026-03-24 20:22:48 +0900

    first

---
+++ src/main/java/org/example/AA.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class AA {
+
+    public void aa(){
+        System.out.print("Dfd");
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        AA a =new AA();
+        a.aa();
+
+
+        InputStreamReader ir= new InputStreamReader( System.in);
+    }
+}
